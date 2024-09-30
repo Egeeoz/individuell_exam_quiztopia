ef677def9666eeeb81e762a84ae1288b2aafdf10
Egeeoz <ozakdemirege@hotmail.com>
2024-09-30 14:37:28 +0200

    create response and error function, create db service and userSignup function

---
+++ functions/userSignup/index.js
@@ -0,0 +1,50 @@
+const { sendResponse, sendError } = require('../../responses/index');
+const { db } = require('../../services/db');
+const bcrypt = require('bcryptjs');
+const { v4: uuidv4 } = require('uuid');
+const { ScanCommand, PutCommand } = require('@aws-sdk/lib-dynamodb');
+
+exports.handler = async (event) => {
+  const { username, password } = JSON.parse(event.body);
+
+  if (!username || !password) {
+    return sendError(400, 'Username and Password are required');
+  }
+
+  try {
+    const scanParams = {
+      TableName: 'quiz',
+      FilterExpression: 'username = :username',
+      ExpressionAttributeValues: {
+        ':username': { S: username },
+      },
+    };
+
+    const scanCommand = new ScanCommand(scanParams);
+    const result = await db.send(scanCommand);
+
+    if (result.Items.length > 0) {
+      return sendError(400, 'Username already exists, pick another Username');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const userId = uuidv4();
+
+    const putParams = {
+      TableName: 'quiz',
+      Item: {
+        id: userId,
+        username: username,
+        password: hashedPassword,
+      },
+    };
+
+    const putCommand = new PutCommand(putParams);
+    await db.send(putCommand);
+
+    return sendResponse('User successfully created');
+  } catch (error) {
+    console.error(error);
+    return sendError(500, 'Internal server error');
+  }
+};
