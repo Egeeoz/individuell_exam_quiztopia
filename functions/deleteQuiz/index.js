bcd7af55fd8096c29d36a712e2e0e6d9313b61bf
Egeeoz <ozakdemirege@hotmail.com>
2024-10-03 11:57:44 +0200

    Add validation to restrict note deletion to its creator

--- functions/deleteQuiz/index.js
+++ functions/deleteQuiz/index.js
@@ -1,17 +1,34 @@
 const middy = require('@middy/core');
 const { validateToken } = require('../../middleware/auth');
 const { db } = require('../../services/db');
-const { DeleteCommand } = require('@aws-sdk/lib-dynamodb');
+const { DeleteCommand, GetCommand } = require('@aws-sdk/lib-dynamodb');
 const { sendResponse, sendError } = require('../../responses/index');
 
 const deleteQuizHandler = async (event) => {
   const id = event.pathParameters?.id;
+  const userId = event.id;
 
   if (!id) {
     return sendError(400, 'Missing quiz ID');
   }
 
   try {
+    const getParams = {
+      TableName: 'quiz',
+      Key: { id: id },
+    };
+
+    const getCommand = new GetCommand(getParams);
+    const result = await db.send(getCommand);
+
+    if (!result.Item) {
+      return sendError(400, 'Quiz not found');
+    }
+
+    if (result.Item.creator !== userId) {
+      return sendError(400, 'You do not have permission to delete this quiz');
+    }
+
     const deleteParams = {
       TableName: 'quiz',
       Key: { id },
